789429e672c004939ec21b812ece5c3f542bcfc8
github username <elis.paul7@gmail.com>
2018-09-17 16:32:59 -0400

    began skills component redesign

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,13 +12,22 @@ import { Grid, Image } from 'semantic-ui-react'
 class Skills extends Component {
   render() {
     return (
-      <div id='skills-container'>
-        {/* <a name="skills" /> */}
-        <div id='skills-title'>
+      <div id='skills-component'>
+        <div id='skills-header'>
           Skills
         </div>
-        <div id='skills-list'>
-          <Grid centered columns={3} >
+        <div id='skills-container'>
+          <div className='skills-title'>Languages</div>
+          <div className='skills-list'>
+            Javascript, Ruby, HTML, CSS, SQL
+          </div>
+
+          <div className='skills-title'>Frameworks</div>
+          <div className='skills-list'>
+            React, React Native, Ruby on Rails, Sinatra, Rack
+          </div>
+
+          {/* <Grid centered columns={3} >
 
             <Grid.Row centered columns={3} >
               <Grid.Column>
@@ -50,7 +59,7 @@ class Skills extends Component {
               </Grid.Column>
             </Grid.Row>
 
-          </Grid>
+          </Grid> */}
         </div>
       </div>
     );
