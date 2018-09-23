98c3abd37f28df865461f3c779f45222b1cc8ac2
github username <elis.paul7@gmail.com>
2018-09-23 16:33:51 -0400

    hero image redesign

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from '../src/components/About'
 import Skills from '../src/components/Skills'
 import Projects from '../src/components/Projects'
 import Footer from '../src/components/Footer'
+import hero from './images/hero.JPG'
 
 // import {  } from 'semantic-ui-react'
 import './App.css';
@@ -20,11 +21,16 @@ class App extends Component {
 
         <div id='app-container'>
           <div id='app-greeting-container'>
-              <div id='app-greeting-title'>
-                Developer.<br />
+              <img src={hero} alt='oh no!'/>
+              {/* <div id='app-greeting-title'>
+                Developer.
               </div>
               <div id='app-greeting-description'>
                 Interested in creating rich user experiences upon a solid foundation of code.
+              </div> */}
+              <div id='app-content'>
+                <h2 id='app-title'>Developer.</h2>
+                <h5 id='app-description'>Interested in creating rich user experiences upon a solid foundation of code.</h5>
               </div>
           </div>
         </div>
