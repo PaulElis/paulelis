d8ac7ce0a3d4fba3c8c7f002b4438709c82fe89c
github username <elis.paul7@gmail.com>
2018-10-06 18:22:17 -0400

    began hamburger icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/header.css';
 import { scroller } from 'react-scroll'
+import hamburger from '../images/hamburger.png'
+import MediaQuery from 'react-responsive';
 
 class Header extends Component {
 
@@ -32,11 +34,16 @@ class Header extends Component {
         <div id='header-title'>
           Paul Elis
         </div>
-        <div className='header-list'>
-          <a className='links' onClick={() => this.scrollToAbout()} >About</a>
-          <a className='links' onClick={() => this.scrollToSkills()} >Skills</a>
-          <a className='links' onClick={() => this.scrollToProjects()} >Projects</a>
-        </div>
+        <MediaQuery query="(max-width: 576px)">
+          <img id='hamburger' src={hamburger} />
+        </MediaQuery>
+        <MediaQuery query="(min-width: 577px)">
+          <div className='header-list'>
+            <a className='links' onClick={() => this.scrollToAbout()} >About</a>
+            <a className='links' onClick={() => this.scrollToSkills()} >Skills</a>
+            <a className='links' onClick={() => this.scrollToProjects()} >Projects</a>
+          </div>
+        </MediaQuery>
       </div>
     );
   }
