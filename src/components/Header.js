5304f3984ac5a56105cc20ff76d050a2a138fc1d
github username <elis.paul7@gmail.com>
2018-10-10 23:09:37 -0400

    seperated ul list

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,24 +34,29 @@ class Header extends Component {
 
   render() {
     return (
+      <div>
       <div id='header-container'>
         <div id='header-title'>
           Paul Elis
         </div>
         <MediaQuery query="(max-width: 576px)">
-          <img id='hamburger' src={hamburger} onClick={this.toggleHamburger}/>
-          <ul class="menulist">
-            <li><a className='links' onClick={() => this.scrollToAbout()} >About</a></li>
-            <li><a className='links' onClick={() => this.scrollToSkills()} >Skills</a></li>
-            <li><a className='links' onClick={() => this.scrollToProjects()} >Projects</a></li>
-          </ul>
+          <img id='hamburger' src={hamburger} alt='oh no!' onClick={this.toggleHamburger}/>
         </MediaQuery>
+
         <MediaQuery query="(min-width: 577px)">
           <div className='header-list'>
             <a className='links' onClick={() => this.scrollToAbout()} >About</a>
             <a className='links' onClick={() => this.scrollToSkills()} >Skills</a>
             <a className='links' onClick={() => this.scrollToProjects()} >Projects</a>
           </div>
+        </MediaQuery>
+      </div>
+        <MediaQuery query="(max-width: 576px)">
+          <ul className="hamburger-list">
+            <a className='links' onClick={() => this.scrollToAbout()} ><li>About</li></a>
+            <a className='links' onClick={() => this.scrollToSkills()} ><li>Skills</li></a>
+            <a className='links' onClick={() => this.scrollToProjects()} ><li>Projects</li></a>
+          </ul>
         </MediaQuery>
       </div>
     );
