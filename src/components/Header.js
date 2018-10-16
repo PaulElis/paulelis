80d385c3e59513f2306d07c96af20191436a955a
github username <elis.paul7@gmail.com>
2018-10-15 20:53:38 -0400

    set state and toggled

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import '../styles/header.css';
 import { scroller } from 'react-scroll'
 import hamburger from '../images/hamburger.png'
-// import exit from '../images/exit.png'
+import exit from '../images/exit.png'
 import MediaQuery from 'react-responsive';
 
 class Header extends Component {
+
+  state={
+    toggled: false
+  }
 
   scrollToAbout() {
     scroller.scrollTo(`scroll-to-about`, {
@@ -30,13 +34,29 @@ class Header extends Component {
   }
 
   openHamburger(){
-    console.log('in hamburger')
+    console.log('in openhamburger')
     document.getElementById('hamburger-list').style.height='200px';
-    // document.getElementById('hamburger').src='../images/exit.png';
+    document.getElementById('hamburger-list').style.width='150px';
+    document.getElementById('app-container').style.marginTop='200px';
+  }
+
+  closeHamburger(){
+    console.log('in closehamburger')
+    document.getElementById('hamburger-list').style.height='0px';
+    document.getElementById('hamburger-list').style.width='0px';
+    document.getElementById('app-container').style.marginTop='0px';
+  }
+
+  toggleHamburger = () => {
+    this.setState({ toggled: !this.state.toggled })
+    document.getElementById('hamburger-list').style.height='200px';
+    document.getElementById('hamburger-list').style.width='150px';
     document.getElementById('app-container').style.marginTop='200px';
   }
 
   render() {
+    let { toggled } = this.state
+
     return (
       <div>
       <div id='header-container'>
@@ -44,7 +64,8 @@ class Header extends Component {
           Paul Elis
         </div>
         <MediaQuery query="(max-width: 576px)">
-          <img id='hamburger' src={hamburger} alt='oh no!' onClick={this.openHamburger}/>
+          {/* <img id='hamburger' src={toggled ? exit : hamburger} alt='oh no!' onClick={this.openHamburger}/> */}
+          <img id='hamburger' src={toggled ? exit : hamburger} alt='oh no!' onClick={this.toggleHamburger}/>
         </MediaQuery>
 
         {/* Web Menu */}
@@ -60,7 +81,7 @@ class Header extends Component {
         {/* Mobile Menu */}
         <MediaQuery query="(max-width: 576px)">
             <div id="hamburger-list">
-              <a id='close-hamburger' onClick={this.closeHamburger}>X</a>
+              {/* <a id='close-hamburger' onClick={this.closeHamburger}>X</a> */}
               <a className='links' onClick={this.scrollToAbout} >About</a>
               <a className='links' onClick={this.scrollToSkills} >Skills</a>
               <a className='links' onClick={this.scrollToProjects} >Projects</a>
