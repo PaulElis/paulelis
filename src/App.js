7ba61ac4740e16497d1864c7d2f5ad646f38b07e
github username <elis.paul7@gmail.com>
2019-01-17 19:54:56 -0500

    added main webp images

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import About from '../src/components/About'
 import Skills from '../src/components/Skills'
 import Projects from '../src/components/Projects'
 import Footer from '../src/components/Footer'
-import main from './images/main/main1500px.jpg'
-import main992 from './images/main/main3000px.jpg'
-import mainXL from './images/main/mainXL.jpg'
+import main from './images/main/main.jpg'
+import mainWebp from './images/main/main.webp'
+import main992px from './images/main/main992px.jpg'
+import main992pxWebp from './images/main/main992px.webp'
+import main1200px from './images/main/main1200px.jpg'
+import main1200pxWebp  from './images/main/main1200px.webp'
 import { Element } from 'react-scroll'
 
 import './App.css';
@@ -21,9 +24,12 @@ class App extends Component {
         </header>
           <div id='app-greeting-container'>
             <picture>
+              <source srcSet={main1200pxWebp} media="(min-width: 1200px)" type="image/webp" />
+              <source srcSet={main992pxWebp} media="(min-width: 992px)" type="image/webp" />
+              <source srcSet={mainWebp} type="image/webp" />
+              <source srcSet={main1200px} media="(min-width: 1200px)" />
+              <source srcSet={main992px} media="(min-width: 992px)" />
               <img src={main} id='main' alt='oh no!'/>
-              <source media="(min-width: 992px)" srcSet={main992} />
-              <source media="(min-width: 1200px)" srcSet={mainXL} />
             </picture>
             <div id='app-content'>
               <h2 id='app-title'>Developer.</h2>
