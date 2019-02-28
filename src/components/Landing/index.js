2bc442db352075f5802d2da5d674e1e55be98c7e
github username <elis.paul7@gmail.com>
2019-02-28 12:34:07 -0500

    cropped landing image resize and position

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './index.css';
 import main from '../../images/main/main.jpg'
 import mainWebp from '../../images/main/main.webp'
-import main992px from '../../images/main/main992px.jpg'
-import main992pxWebp from '../../images/main/main992px.webp'
 import main1200px from '../../images/main/main1200px.jpg'
 import main1200pxWebp  from '../../images/main/main1200px.webp'
 
@@ -17,11 +15,9 @@ class Landing extends Component {
     return (
       <div id='app-greeting-container'>
         <picture>
-          <source srcSet={main1200pxWebp} media="(min-width: 1200px)" type="image/webp" />
-          <source srcSet={main992pxWebp} media="(min-width: 992px)" type="image/webp" />
+          <source srcSet={main1200pxWebp} media="(min-width: 992px)" type="image/webp" />
           <source srcSet={mainWebp} type="image/webp" />
-          <source srcSet={main1200px} media="(min-width: 1200px)" />
-          <source srcSet={main992px} media="(min-width: 992px)" />
+          <source srcSet={main1200px} media="(min-width: 992px)" />
           <img src={main} id='main' alt='oh no!' className='fade-in' />
         </picture>
       </div>
